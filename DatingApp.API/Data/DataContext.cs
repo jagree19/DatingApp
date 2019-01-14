ebb47b063757fa831398f5da9d048f29bb534e6a
Justin Greene <justingreene@Justins-iMac.local>
2019-01-13 21:50:38 -0700

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        private DbSet<Value> _myProperty;
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
